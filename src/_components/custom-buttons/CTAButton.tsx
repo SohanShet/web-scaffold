87ca7a74f7545a4a1645c3346dc365b0e5470e71
Sohan Shet <sshet.builds@gmail.com>
2026-06-10 21:19:47 +0530

    fix: minor fix CTAbutton not disabearing in navbar

--- src/_components/custom-buttons/CTAButton.tsx
+++ src/_components/custom-buttons/CTAButton.tsx
@@ -16,7 +16,7 @@ type NativeButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
 type ButtonProps = LinkButtonProps | NativeButtonProps;
 
 export function CTAButton({ href, children, className = '', ...props }: ButtonProps) {
-	const baseStyles = "inline-flex h-10 items-center justify-center rounded-md bg-primary px-6 py-2 text-sm font-medium text-primary-foreground border border-primary transition-all hover:bg-muted hover:text-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 active:scale-95 shadow-sm";
+	const baseStyles = " h-10 items-center justify-center rounded-md bg-primary px-6 py-2 text-sm font-medium text-primary-foreground border border-primary transition-all hover:bg-muted hover:text-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 active:scale-95 shadow-sm";
 
 	const combinedClassName = `${baseStyles} ${className}`;
 
