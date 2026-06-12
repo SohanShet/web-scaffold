ebdd59ad227f642b9bd995e7a6661c3e2b6f84e1
Sohan Shet <sshet.builds@gmail.com>
2026-06-13 00:36:42 +0530

    feat: add zod dependency

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,7 +1,9 @@
 import { MetadataRoute } from "next";
+import { env } from "@/../env";
+
 
 export default function robots(): MetadataRoute.Robots {
-	const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || "https://webscaffold.com";
+	const baseUrl = env.NEXT_PUBLIC_SITE_URL;
 
 	return {
 		rules: {
